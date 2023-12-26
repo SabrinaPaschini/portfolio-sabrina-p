50d4c96c5f56ecfba98d7c9508b739ddc74a23ce
Sabrina Paschini <sabrina.paschini@hotmail.com>
2023-12-26 09:56:10 -0300

    rename-json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,6 @@
-function fetchProfileData(){
-    const url = 
-    fetch{}
+async function  fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/SabrinaPaschini/potifolio/main/assets/data/profile.json'
+    const fetching = await fetch{url}
+    return await fetching.json()
 }
+
