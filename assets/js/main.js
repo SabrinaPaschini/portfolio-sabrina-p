18fe5695b2263b81ad44613f5a75757159d2899d
Sabrina Paschini <sabrina.paschini@hotmail.com>
2023-12-30 11:41:15 -0300

    profile

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,7 +61,7 @@ function updatePortifolio(profileData) {
         return ` 
         
         <li>
-            <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
+            <h3 ${project.github ? 'class="github"' : ''}> ${project.name}</h3>
             <a href="${project.url}" target="_blank">${project.url}</a>
         </li>
         `
