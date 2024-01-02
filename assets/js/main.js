bb11b06f7bd4213efbb380f227169cea6ffd5dd3
Sabrina Paschini <sabrina.paschini@hotmail.com>
2024-01-02 13:59:15 -0300

    icons-change-place

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,9 +59,9 @@ function updatePortfolio(profileData) {
     portfolio.innerHTML = profileData.portfolio.map(project => {
         return ` 
         <li>
-            <h3 ${project.github ? 'class="github"' : ''}> ${project.name}</h3>
-            <a href="${project.url}" target="_blank">${project.url}</a>
+            <h3 ${project.github ? 'class="github"' : ''}> ${project.name}:</h3>
         </li>
+        <li><a href="${project.url}" target="_blank">${project.url}</a></li>
         `
     }).join('')
 }
